d3ce50d14640d56f2cca56ab7959df55483e6b87
pooja allampallewar <poojasa718@gmail.com>
2023-03-11 12:34:41 +0530

    Define constants in used in Verilog code

---
+++ constants.py
@@ -0,0 +1,42 @@
+'''
+Define constants in used in Verilog code 
+'''
+
+
+WHITESPACE = ' \t\n\r'
+
+SYMBOLS = '!@#$%^&*()_-+={}[]|\:;"<>,.?/~`'
+
+OPERATORS = ['+',
+             '-',
+             '*',
+             '/',
+             '%',
+             '&',
+             '|',
+             '^',
+             '~',
+             '<<',
+             '>>'
+             ]
+
+KEYWORDS = ['module',
+            'endmodule',
+            'input',
+            'output',
+            'wire',
+            'reg',
+            'assign',
+            'always',
+            'if',
+            'else',
+            'while',
+            'for',
+            'begin',
+            'end',
+            'integer',
+            'case',
+            'endcase',
+            'default',
+            'function'
+            ]
